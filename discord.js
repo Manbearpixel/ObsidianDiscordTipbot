7f8177fde17b113ceb7f0e475473e4c096569776
Manbearpixel <manbearpixel@gmail.com>
2017-12-04 02:37:00 -0600

    Made notifications optional

--- discord.js
+++ discord.js
@@ -19,8 +19,11 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
+  
   client.user.setGame(`. Type !help for Tipbot`);
-  Notifications.setupNotifications(client);
+  if (settings.hasOwnProperty('notifications') && settings.notifications === true) {
+    Notifications.setupNotifications(client);
+  }
 });
 
 client.on("guildCreate", guild => {
